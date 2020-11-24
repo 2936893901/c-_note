f8524e6f1f3dfa736393d1ab17018901ad1ed3f3
lose-dreams <54893589+verloren-droom@users.noreply.github.com>
2020-11-24 20:36:53 +0800

    Update Resources.h

--- exercise/actual_combat/smart_ptr/Resources.h
+++ exercise/actual_combat/smart_ptr/Resources.h
@@ -1,12 +1,12 @@
 #ifndef RESOURCES_H
 #define RESOURCES_H
 
-// 书类
+// 测试类
 class Test
 {
 public:
-	Test(){std::cout << "Test()";}
-	~Test(){std::cout << "~Test()";}
+	Test(){std::cout << "Test()\n";}
+	~Test(){std::cout << "~Test()\n";}
 };
 
 #endif
